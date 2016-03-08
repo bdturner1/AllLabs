b8e79b3df638bc808e08643a969421c59c570803
blake <blake.turner352@myci.csuci.edu>
2016-03-07 21:16:03 -0800

    finally turing in lab6!

---
+++ Lab6/MavenPlatformWordApp/MavenWordEngine/target/generated-sources/annotations/com/mycompany/mavenwordengine/Bundle.java
@@ -0,0 +1,27 @@
+package com.mycompany.mavenwordengine;
+/** Localizable strings for {@link com.mycompany.mavenwordengine}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Text</i>
+     * @see TextTopComponent
+     */
+    static String CTL_TextAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_TextAction");
+    }
+    /**
+     * @return <i>Text Window</i>
+     * @see TextTopComponent
+     */
+    static String CTL_TextTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_TextTopComponent");
+    }
+    /**
+     * @return <i>This is a Text window</i>
+     * @see TextTopComponent
+     */
+    static String HINT_TextTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_TextTopComponent");
+    }
+    private void Bundle() {}
+}
